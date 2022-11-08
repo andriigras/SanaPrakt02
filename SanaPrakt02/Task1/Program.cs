57f9564a4c8ace0e2e99996d1c36e5779c96fe82
Andrii Gras <sana22_gav@student.ztu.edu.ua>
2022-11-08 15:47:38 +0200

    text changed at 3 lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Andrii Gras");
+Console.WriteLine("Telephone");
+Console.WriteLine("I have");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
